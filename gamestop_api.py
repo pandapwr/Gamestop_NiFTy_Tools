78a7e69f61629167dd36c4813edf2b3d07d3b598
pandapwr <r34p3rex@gmail.com>
2022-07-24 16:31:25 -0400

    get_owned_nfts_value uses collection floor price for 1/1 items that are not for sale

--- gamestop_api.py
+++ gamestop_api.py
@@ -1,6 +1,6 @@
 import requests
 from datetime import datetime
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor
 
 API_HEADERS = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
