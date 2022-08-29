24732f319588e0f6159f8ae2066de8a18a384f89
buzzCraft <67235613+buzzCraft@users.noreply.github.com>
2022-08-29 16:24:54 +0200

    Reverting hotfix

--- nifty_database.py
+++ nifty_database.py
@@ -188,7 +188,7 @@ class NiftyDB:
         result = self.c.fetchone()
         if result is None:
             print(f"No historical price data found for {currency} at {timestamp}")
-            return 1
+            return None
         else:
             return result['price']
 
